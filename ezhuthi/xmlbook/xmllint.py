ae3ce496fbd0ee073eaec802aa2c8e2fedbdae51
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2023-05-17 14:42:14 +0530

    ezhil-Win x86

---
+++ ezhuthi/xmlbook/xmllint.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# (C) 2017 Ezhil Language Foundation
+# 
+from __future__ import print_function
+from xml.dom.minidom import parse
+import sys
+import glob
+
+def do_check(args):
+    for f in args:
+        data = ('parsing ... %s'%f)
+        try:
+            parse(f)
+        except Exception as ie:
+            print(str(ie))
+            print(data+' ... FAIL')
+            continue
+        print(data+' ... OK')
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        do_check(sys.argv[1:])
+    else:
+        do_check(glob.glob("*.xml"))
